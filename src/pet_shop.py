15df9ebc225216bef18e5ec19b52a04fbe0badd4
Andrej Mihalylovics <andrej.mihalylovics@gmail.com>
2020-11-21 19:30:25 +0000

    completed the first 6 tests

---
+++ src/pet_shop.py
@@ -0,0 +1,29 @@
+# WRITE YOUR FUNCTIONS HERE
+
+def get_pet_shop_name(shop):
+    return shop["name"]
+
+def get_total_cash(shop):
+    return shop["admin"]["total_cash"] 
+
+def add_or_remove_cash(shop, cash_to_add_or_remove):
+    shop["admin"]["total_cash"] += cash_to_add_or_remove 
+
+def add_or_remove_cash(shop, cash_to_add_or_remove):
+    shop["admin"]["total_cash"] += cash_to_add_or_remove 
+
+def get_pets_sold(amount):
+    return amount["admin"]["pets_sold"]
+
+
+def increase_pets_sold(amount_1, amount_2):
+    amount_1["admin"]["pets_sold"] += amount_2
+    
+
+        
+
+
+        
+
+
+    
