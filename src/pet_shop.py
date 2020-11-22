089ba0bca62dd294c83dc0ce3b2100e5dbd33021
Andrej Mihalylovics <andrej.mihalylovics@gmail.com>
2020-11-22 16:38:12 +0000

    2 left

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -21,17 +21,63 @@ def increase_pets_sold(amount_1, amount_2):
     
 def get_stock_count(shop):
     return len(shop["pets"])
-
+    
 
 def get_pets_by_breed (pet_shop, breed):
     pets_by_breed = []
-    for pet in pet_shop ["pets"]:
-        if pet ["breed"] == breed:
+    for pet in pet_shop["pets"]:
+        if pet["breed"] == breed:
             pets_by_breed.append(pet)
     return pets_by_breed
 
 
-        
+
+def get_pets_by_breed (shop, breed):
+    pets_by_breed = []
+    for pet in shop["pets"]:
+        if pet["breed"] == breed:
+            pets_by_breed.append(pet)
+    return pets_by_breed
+
+
+def find_pet_by_name (shop, pet_name):
+    for pet in shop["pets"]:
+        if pet["name"] == pet_name:
+            return pet
+
+
+def find_pet_by_name(shop, pet_name):
+    for pet in shop["pets"]:
+        if pet["name"] == pet_name:
+            return pet
+
+
+
+
+#  def remove_pet_by_name(shop,pet_name):
+#     for pet in shop["pets"]:
+#         if pet["name"] == pet_name:
+#             del pet
+
+
+
+def add_pet_to_stock(shop, new_pet_1):
+    shop["pets"].append(new_pet_1)
+
+
+def get_customer_cash(costumer):
+    return costumer["cash"]
+
+def remove_customer_cash(customer, cash_remove):
+    customer["cash"] -= cash_remove
+
+
+
+
+
+
+
+
 
 
 
